25b947f199f86f1b2b35f91b5063edc2179cc051
navib <ivan.baena.dev@gmail.com>
2018-03-18 22:13:41 -0400

    fixes

--- src/api/quotes.js
+++ src/api/quotes.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import actions from "../actions";
 
 const makeRequestQuote = () =>
-  axios.get("https://api.myjson.com/bins/157ovn", {
+  axios.get("https://api.myjson.com/bins/fr71v", {
     withCredentials: false
   });
 
